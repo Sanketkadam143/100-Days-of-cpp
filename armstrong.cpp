66ceb96799a337139e694836781c6a2d5c603e19
Sanket <sanketanilkadam@gmail.com>
2022-04-14 15:28:17 +0530

    added armstrong

---
+++ armstrong.cpp
@@ -0,0 +1,28 @@
+// Armstrong number is a number that is equal to the sum of cubes of its digits.
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n,sum=0,temp,r,size;
+    cin>>n>>size;
+    temp=n; 
+
+while (n>0)
+{
+   r=n%10;
+   sum+=pow(r,size);
+   n=n/10;
+
+}
+if (temp==sum)
+{
+   cout<<"Its a armstrong";
+}
+else
+{
+    cout<<"Its not a armstrong";
+}
+  
+  return 0;
+}
