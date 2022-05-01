56b746766f18985d48560f0068a5723238e22221
Sanket Kadam <83902364+Sanketkadam143@users.noreply.github.com>
2022-05-02 00:11:21 +0530

    Update arraysize.cpp

--- arraysize.cpp
+++ arraysize.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int main()
+ {
   int arr[] = {10,20,30,40,50,60};
   int arrSize = sizeof(arr)/sizeof(arr[0]);
   cout << "The size of the array is: " << arrSize;
