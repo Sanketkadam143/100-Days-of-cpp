3d4051a26eabf4deead285de1000fe71d0f6765e
Sanket <sanketanilkadam@gmail.com>
2022-05-04 14:28:47 +0530

    added integer sum from string

---
+++ add_integers_from_string.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string s;
+  getline(cin,s);
+    
+   int digit,sum=0;
+
+
+   stringstream ss(s);
+   
+   while(ss>>digit){
+        
+      sum+=digit;
+      
+   }
+   cout<<sum;
+
+   return 0;
+}
